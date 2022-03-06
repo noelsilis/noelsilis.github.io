cea4c3ca26b563754774e230b81ee9d85449d2dd
noelsilis <noelsilis99@gmail.com>
2022-03-05 18:16:11 -0600

    Pasteleria

--- js/index.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(){
-    if($("#menu").offset().top>56) {
-        $("#menu").addClass("nav-color navbar-dark");
-    }else{
-        $("#menu").removeClass("nav-color navbar-dark");
-    }
- });
